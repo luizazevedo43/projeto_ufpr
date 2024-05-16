25411440cf5319817f0166a7615c5b6c2939b129
unknown <luizazevedo43@gmail.com>
2024-05-15 22:41:17 -0300

    teste 1 railway

--- setup/settings.py
+++ setup/settings.py
@@ -57,7 +57,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage' 
 
 ROOT_URLCONF = 'setup.urls'
 
